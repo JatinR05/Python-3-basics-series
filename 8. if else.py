e4b3d8056e2074602c9ed0cd201676484dd0d179
JatinR05 <71865805+JatinR05@users.noreply.github.com>
2020-09-30 22:35:16 +0530

    Update 8. if else.py

--- 8. if else.py
+++ 8. if else.py
@@ -19,9 +19,11 @@ x = 5
 y = 8
 
 if x < y:
+    print('y is greater than x')
+else:
     print('x is greater than y')
 if x > 55:
     print('x is greater than 55')
 else:
-    print('x is not greater than y or 55')
+    print('x is not greater than 55')
 
